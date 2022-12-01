5c01fd738d6a83409323ba9fc482e03b3a247275
ks23.code+databrick1@gmail.com <ks23.code+databrick1@gmail.com>
2022-12-01 05:25:02 +0000

    0.1/4

--- sql/ETL with Spark SQL & Python.sql
+++ sql/ETL with Spark SQL & Python.sql
@@ -1,2 +1,33 @@
 -- Databricks notebook source
+-- MAGIC %run ../Copy-Datasets
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC files = dbutils.fs.ls(f"{dataset_bookstore}/customers-json")
+-- MAGIC display(files)
+
+-- COMMAND ----------
+
+SELECT * FROM json.`${dataset.bookstore}/customers-json/export_001.json`
+
+-- COMMAND ----------
+
+-- all file with specific wild card as mention below
+SELECT * FROM json.`${dataset.bookstore}/customers-json/export_*.json`
+
+
+--  NOTE that we will be geting only 1000 rows @first 1000rows
+
+-- COMMAND ----------
+
+--  total directory files @note that all files should have same file format and the schema
+SELECT * FROM json.`${dataset.bookstore}/customers-json`
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM json.`${dataset.bookstore}/customers-json`
+
+-- COMMAND ----------
+
 
